aa4840edf589c20492bddb74c1f65c7d61e943a9
ivs-silva <ivonne.silvah@gmail.com>
2024-03-08 12:14:13 +0100

    update formatted date

--- src/FormatDateForecast.js
+++ src/FormatDateForecast.js
@@ -3,11 +3,7 @@ import React from "react";
 export default function FormatDateForecast(props) {
     let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
     let day = days[props.date.getDay()];
-    let hours = props.date.getHours();
-    let minutes = props.date.getMinutes();
-    if (minutes < 10){
-        minutes= `0${minutes}`;
-    }
+   
 
     return <div>{day}</div>;
 }
